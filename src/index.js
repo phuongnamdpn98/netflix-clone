c6204a65b3b37ce412875636474288d7d3069e95
Nam Dang Dev <116860706+phuongnamdpn98@users.noreply.github.com>
2024-12-21 16:20:26 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,9 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
-    
+    <App />  
   </Router>
 );
-
-
